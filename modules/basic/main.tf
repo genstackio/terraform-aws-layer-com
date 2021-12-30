348f737fc503682627a547306048d5c9e43a754a
Olivier Hoareau <oha@greenberets.io>
2021-12-30 18:59:58 +0100

    chore: convert to use provider constraints

--- modules/basic/main.tf
+++ modules/basic/main.tf
@@ -1,13 +1,13 @@
 module "ses-global" {
   source  = "genstackio/ses/aws//modules/global"
-  version = "0.1.0"
+  version = "0.2.0"
   domain  = var.dns
   zone    = var.zone
 }
 
 module "ses-regional-identity" {
   source          = "genstackio/ses/aws//modules/regional-identity"
-  version         = "0.1.0"
+  version         = "0.2.0"
   name            = "${var.env}-${replace(var.dns, ".", "-")}"
   sources         = var.sources
   service_sources = var.service_sources
@@ -20,7 +20,7 @@ module "ses-regional-identity" {
 }
 module "ses-global-verification" {
   source          = "genstackio/ses/aws//modules/global-verification"
-  version         = "0.1.0"
+  version         = "0.2.0"
   domain          = var.dns
   zone            = var.zone
   identities      = local.identities
@@ -31,7 +31,7 @@ module "ses-global-verification" {
 
 module "ses-regional-verification" {
   source    = "genstackio/ses/aws//modules/regional-verification"
-  version   = "0.1.0"
+  version   = "0.2.0"
   id        = module.ses-regional-identity.id
   providers = {
     aws = aws
@@ -42,6 +42,6 @@ module "ses-regional-verification" {
 module "ses-smtp-user" {
   count   = null != var.smtp_user_name ? 1 : 0
   source  = "genstackio/ses/aws//modules/smtp-user"
-  version = "0.1.0"
+  version = "0.2.0"
   name    = var.smtp_user_name
 }
