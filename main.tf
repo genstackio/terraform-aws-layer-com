1de676e9f9ba8bbd88575ad7967bcad150a43293
Olivier Hoareau <oha@greenberets.io>
2022-05-30 12:19:57 +0200

    chore: up to compatibility to tf 1.2.1 + aws provider 4.16.0

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "ses-global" {
   source  = "genstackio/ses/aws//modules/global"
-  version = "0.2.0"
+  version = "0.3.0"
   domain  = var.dns
   zone    = var.zone
 }
 
 module "ses-regional-identity" {
   source          = "genstackio/ses/aws//modules/regional-identity"
-  version         = "0.2.0"
+  version         = "0.3.0"
   name            = "${var.env}-${replace(var.dns, ".", "-")}"
   sources         = var.sources
   service_sources = var.service_sources
@@ -17,7 +17,7 @@ module "ses-regional-identity" {
 }
 module "ses-regional-identity-shared" {
   source          = "genstackio/ses/aws//modules/regional-identity"
-  version         = "0.2.0"
+  version         = "0.3.0"
   name            = "${var.env}-${replace(var.dns, ".", "-")}"
   sources         = var.sources
   service_sources = var.service_sources
@@ -31,7 +31,7 @@ module "ses-regional-identity-shared" {
 
 module "ses-global-verification" {
   source          = "genstackio/ses/aws//modules/global-verification"
-  version         = "0.2.0"
+  version         = "0.3.0"
   domain          = var.dns
   zone            = var.zone
   identities      = local.identities
@@ -39,13 +39,13 @@ module "ses-global-verification" {
 
 module "ses-regional-verification" {
   source    = "genstackio/ses/aws//modules/regional-verification"
-  version   = "0.2.0"
+  version   = "0.3.0"
   id        = module.ses-regional-identity.id
   depends_on = [module.ses-global-verification]
 }
 module "ses-regional-verification-shared" {
   source    = "genstackio/ses/aws//modules/regional-verification"
-  version   = "0.2.0"
+  version   = "0.3.0"
   id        = module.ses-regional-identity-shared.id
   providers = {
     aws = aws.shared
@@ -55,7 +55,7 @@ module "ses-regional-verification-shared" {
 
 module "pinpoint-app" {
   source    = "genstackio/pinpoint/aws"
-  version   = "0.1.1"
+  version   = "0.2.0"
   name      = "${var.env}-${replace(var.dns, ".", "-")}"
   email     = null != var.pinpoint_channels.email ? {from = "${var.identities[var.pinpoint_channels.email.identity]}@${var.dns}", identity = module.ses-regional-identity-shared.arn} : null
   sms       = null != var.pinpoint_channels.sms ? {} : null
--- modules/basic/main.tf
+++ modules/basic/main.tf
@@ -1,47 +1,38 @@
 module "ses-global" {
   source  = "genstackio/ses/aws//modules/global"
-  version = "0.2.0"
+  version = "0.3.0"
   domain  = var.dns
   zone    = var.zone
 }
 
 module "ses-regional-identity" {
   source          = "genstackio/ses/aws//modules/regional-identity"
-  version         = "0.2.0"
+  version         = "0.3.0"
   name            = "${var.env}-${replace(var.dns, ".", "-")}"
   sources         = var.sources
   service_sources = var.service_sources
   domain          = var.dns
   zone            = var.zone
   emails          = var.identities
-  providers = {
-    aws = aws
-  }
 }
 module "ses-global-verification" {
   source          = "genstackio/ses/aws//modules/global-verification"
-  version         = "0.2.0"
+  version         = "0.3.0"
   domain          = var.dns
   zone            = var.zone
   identities      = local.identities
-  providers = {
-    aws = aws
-  }
 }
 
 module "ses-regional-verification" {
   source    = "genstackio/ses/aws//modules/regional-verification"
-  version   = "0.2.0"
+  version   = "0.3.0"
   id        = module.ses-regional-identity.id
-  providers = {
-    aws = aws
-  }
   depends_on = [module.ses-global-verification]
 }
 
 module "ses-smtp-user" {
   count   = null != var.smtp_user_name ? 1 : 0
   source  = "genstackio/ses/aws//modules/smtp-user"
-  version = "0.2.0"
+  version = "0.3.0"
   name    = var.smtp_user_name
 }
