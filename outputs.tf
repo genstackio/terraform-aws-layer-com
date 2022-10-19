e967e198c0429cc779f893baa60c3dcda9346a40
Olivier Hoareau <oha@greenberets.io>
2022-10-19 19:17:46 +0200

    fix: fix

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "identities" {
   value = local.identities
 }
+output "flatten_identities" {
+  value = local.flatten_identities
+}
+output "flatten_identities_shared" {
+  value = local.flatten_identities_shared
+}
